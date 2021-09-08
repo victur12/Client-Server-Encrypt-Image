1eecae7ab53cda21e2af7d9b27c68c30cb09c75b
victur12 <vgonzalezvictor0@gmail.com>
2021-09-07 20:58:37 -0500

    la llave ahora acepta string

--- Cliente.py
+++ Cliente.py
@@ -5,7 +5,7 @@ def socket():
     cliente.connect(('localhost', 8050))
 
     
-    file = open('encriptado.png', 'rb')
+    file = open('I:/UAQ/Quinto/SD/Proyecto/github/Client-Server-Encrypt-Image/encriptado.png', 'rb')
 
     image_data  = file.read(2048)
 
@@ -20,49 +20,51 @@ def socket():
 def encriptar():
     #Asignamos los valores de la llave
     
-    key = 27
-    
-   
-    print('Key:', key)
+    key = "123"
     
    
     try:
-        # le mandamos el path de la imagen que queremos encriptar
-        path = "I:/UAQ/Quinto/SD/Proyecto/encriptado.png"
+            # le mandamos el path de la imagen que queremos encriptar
+        path = "I:/UAQ/Quinto/SD/Proyecto/github/Client-Server-Encrypt-Image/encriptado.png"
+            
+            # pedimos la llave para poder encriptar
+        key =input('Ingresa el valor de la llave para encriptar : ')
+        key = bytes(key, 'ascii')
         
-        # pedimos la llave para poder encriptar
-        key = int(input('Ingresa el valor de la llave para encriptar : '))
-        
+
+
         # imprimimos la direccion de la imagen y la llave 
         print('El path de la imagen es: ', path)
         print('la llave es: ', key)
-        
+
         # Abrimos el archivo solo para leerlo
         fin = open(path, 'rb')
-        
+
         # guardamos los datos de la imagen en imagen
         imagen = fin.read()
         fin.close()
-        
+
         # convertimos la imagen en un array de bytes
         # para realizar mas facilmente el cifrado
         imagen = bytearray(imagen)
-    
+
         # realizamos la operacion XOR en cada valor del array
         for index, values in enumerate(imagen):
-            imagen[index] = values ^ key
-    
+            for byte in key:
+                imagen[index] = imagen[index] ^ byte
+
         # abrimos el archivo con el proposito de escribir
         fin = open(path, 'wb')
-        
+
         # escribimos la imagen encriptada
         fin.write(imagen)
         fin.close()
         print('encriptacion hecha...')
-    
-        
-    except Exception:
-        print('Error: ', Exception.__name__)
+
+    except Exception :
+        print('Error', Exception.__name__)
+
+  
 
 def copiar():
     
--- Server.py
+++ Server.py
@@ -33,11 +33,11 @@ def desencriptar():
     
     try:
         # tomamos el path de la imagen a desencriptar
-        path = "I:/UAQ/Quinto/SD/Proyecto/server_image.png"
+        path = "I:/UAQ/Quinto/SD/Proyecto/github/Client-Server-Encrypt-Image/server_image.png"
         
         # pedimos el valor de la llave
-        key = int(input('Ingresa el valor de la llave para encriptar : '))
-        
+        key =input('Ingresa el valor de la llave para encriptar : ')
+        key = bytes(key, 'ascii')
         # Imprimo los valores que estamos ocupando para verificar que este bien
         print('la direccion de la imagen es: ', path)
         print('Como es encriptacion simetrica la key debe de ser la misma')
@@ -52,11 +52,12 @@ def desencriptar():
         
         #  convertimos la imagen en un array de bytes
         imagen = bytearray(imagen)
-    
+
         # abrimos el archivo con el proposito de escribir
         for index, values in enumerate(imagen):
-            imagen[index] = values ^ key
-    
+            for byte in key:
+                imagen[index] = imagen[index] ^ byte
+
         # opening file for writing purpose
         fin = open(path, 'wb')
         
@@ -64,10 +65,11 @@ def desencriptar():
         fin.write(imagen)
         fin.close()
         print('desencriptacion hecha...')
-    
-    
+
     except Exception:
         print('Error: ', Exception.__name__)
+    
+
 
 """
 Tienen que crear una funcion que reenvie la imagen ya desencriptada al cliente para que este pueda verificar si son
